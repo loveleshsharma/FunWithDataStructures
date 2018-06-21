55b602676873ac8656cf5c0b12cef4deb1e4a93f
Lovelesh <lovelesh90@gmail.com>
2018-06-21 22:45:12 +0530

    Completed Trie Data Structure with insert and search utility methods

--- src/Implementations/Trie.java
+++ src/Implementations/Trie.java
@@ -2,27 +2,52 @@ package Implementations;
 
 public class Trie {
 
-    private TrieNode root;
+    private final TrieNode root;
 
-
-
-
-}
-
-class TrieNode {
-    private TrieNode[] children = new TrieNode[26];
-    private boolean isWord;
-
-    void insert(String key) {
-        int len = key.length();
-        int index = 0;
-        for (int i = 0; i < len; i++) {
-            index = key.charAt(i) - 'a';
-            if(children[index] == null)
-                children[index] = new TrieNode();
-
-        }
+    public Trie() {
+        this.root = new TrieNode();
     }
 
+    public void insert(String key) {
+        int len = key.length();
+        int index = 0;
+        TrieNode crawler = root;
+        for (int i = 0; i < len; i++) {
+            index = key.charAt(i) - 'a';
+            if(crawler.children[index] == null)
+                crawler.children[index] = new TrieNode();
+            crawler = crawler.children[index];
+        }
+        crawler.isWord = true;
+    }
 
+    public boolean search(String key) {
+        int len = key.length();
+        int index=0;
+        TrieNode crawler = root;
+        for (int i = 0; i < len; i++) {
+            index = key.charAt(i) - 'a';
+            if(crawler.children[index] == null)
+                return false;
+            crawler = crawler.children[index];
+        }
+        return (crawler != null && crawler.isWord);
+    }
+
+    static class TrieNode {
+        final TrieNode[] children = new TrieNode[26];
+        boolean isWord;
+    }
+
+    public static void main(String...args) {
+        Trie trie = new Trie();
+        trie.insert("abcd");
+        trie.insert("abcc");
+        trie.insert("abcde");
+        trie.insert("abbc");
+        trie.insert("abcd");
+        System.out.println(trie.search("f"));
+    }
 }
+
+
